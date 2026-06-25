6c9d6d820f42f745e4243ca651c145006f85dec5
trendywebworks <trendywebworks@gmail.com>
2026-06-25 18:57:15 +0530

    style fix for cpt

--- single-education.php
+++ single-education.php
@@ -1,13 +1,11 @@
 <?php
 $template = get_page_template_slug(get_the_ID());
+$slug = get_post_field('post_name', get_the_ID());
 
-switch ($template) {
-    case 'what-is-sepsis.php':
-        get_template_part('template-parts/education/what-is-sepsis');
-        break;
-
-    default:
-        get_template_part('template-parts/education/default');
-        break;
+if ($slug === 'what-is-sepsis' || $template === 'template-parts/education/what-is-sepsis.php') {
+    get_template_part('template-parts/education/what-is-sepsis');
+    return;
 }
+
+get_template_part('template-parts/education/default');
 ?>
